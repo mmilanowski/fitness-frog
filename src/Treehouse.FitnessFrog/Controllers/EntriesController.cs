e1190a4c4c3c6c22b8a04a1e3edc74147726d51f
mattm <mmilanowski@yahoo.com>
2017-01-04 11:57:34 -0600

    access denied? Saving incase something bad happens.

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -52,7 +52,24 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
-                //TODO Display the Entires list page. 
+
+                List<Entry> entries = _entriesRepository.GetEntries();
+
+                // Calculate the total activity.
+                double totalActivity = entries
+                    .Where(e => e.Exclude == false)
+                    .Sum(e => e.Duration);
+
+                // Determine the number of days that have entries.
+                int numberOfActiveDays = entries
+                    .Select(e => e.Date)
+                    .Distinct()
+                    .Count();
+
+                ViewBag.TotalActivity = totalActivity;
+                ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+                return View("Index", entries);
             }
 
 
